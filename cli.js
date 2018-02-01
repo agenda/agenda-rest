3fbf0ed98ac9cc7340b1f7a4de7142f8ad28e685
Alexis Tyler <xo@wvvw.me>
2018-02-01 22:42:39 +1030

    cleanup cli

--- cli.js
+++ cli.js
@@ -1,32 +1,20 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const app = require('./dist/index');
+const {app} = require('./dist/index');
 
 program
     .option('-d, --dbname <dbname>', '[optional] Name of the Mongo database')
     .option('-h, --dbhost <dbhost>', '[optional] Mongo instance\'s IP')
     .option('-p, --port <port>', '[optional] Server port, default 4040', (n, d) => Number(n) || d, 4040)
-    .option('-t, --timeout <timeout>', '[optional] Timeout for request duration',
-        (n, d) => Number(n) || d, 5000)
+    .option('-t, --timeout <timeout>', '[optional] Timeout for request duration', (n, d) => Number(n) || d, 5000)
     .parse(process.argv);
 
 const settings = require('./dist/settings');
 
-if (program.dbname) {
-  settings.dbname = program.dbname;
-}
-
-if (program.dbhost) {
-  settings.dbhost = program.dbhost;
-} else if (!settings.dbhost) {
-  console.error('neither \'--dbhost\' nor \'$DB_HOST env\' were present');
-  process.exit(1);
-}
-
-if (program.timeout) {
-  settings.timeout = program.timeout;
-}
+settings.dbname = program.dbname || settings.dbname;
+settings.dbhost = program.dbhost || settings.dbhost;
+settings.timeout = program.timeout || settings.timeout;
 
 app.listen(program.port, () => {
   console.log(`App listening on port ${program.port}.`);
