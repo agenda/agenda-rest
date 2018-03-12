752ce076062a20661078f09e0b568b13373fd26e
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2018-03-12 14:58:18 +0330

    tests for simple scenarios of now and every

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const {promisify} = require('util');
 const test = require('ava');
 const request = require('supertest');
+const {range} = require('pythonic');
 const {bootstrapKoaApp} = require('./src/util');
 
 const agendaAppUrl = 'http://localhost:4041';
@@ -9,29 +10,6 @@ const {app: testApp, router: testAppRouter} = bootstrapKoaApp();
 const getTestAppUrl = path => path ? `${testAppUrl}${path}` : testAppUrl;
 
 const agendaAppRequest = request(agendaAppUrl);
-
-testAppRouter.post('/foo', async (ctx, next) => {
-  console.log('foo invoked!');
-  ctx.body = 'foo success';
-  ctx.status = 200;
-  await next();
-});
-
-testAppRouter.post('/foo/:fooParam', async (ctx, next) => {
-  console.log('foo with params invoked!');
-  console.log(ctx.params);
-  console.log(ctx.request.body);
-  ctx.body = 'foo with params success';
-  ctx.status = 200;
-  await next();
-});
-
-testAppRouter.post('/foo/cb', async (ctx, next) => {
-  console.log('foo callback invoked!');
-  ctx.body = 'foo callback success';
-  ctx.status = 200;
-  await next();
-});
 
 const bootstrapApp = async () => {
   const {app, jobsReady} = require('./src');
@@ -75,6 +53,60 @@ test.serial('PUT /api/job succeeds when the job exists', async t => {
     .send({url: getTestAppUrl('/foo')});
 
   t.is(res.status, 200);
+});
+
+const testAppCalled = async (defineEndpoint, thisManyTimes = 1) => {
+  const calls = range(thisManyTimes).map(() => new Promise(resolve => defineEndpoint(resolve)));
+  return Promise.all(calls);
+};
+
+const defineEndpointFoo = done => testAppRouter.post('/foo', async (ctx, next) => {
+  console.log('foo invoked!');
+  ctx.body = 'foo success';
+  ctx.status = 200;
+  done();
+  await next();
+});
+/* TODO
+const defineEndpointFooParam = done => testAppRouter.post('/foo/:fooParam', async (ctx, next) => {
+  console.log('foo with params invoked!');
+  console.log(ctx.params);
+  console.log(ctx.request.body);
+  ctx.body = 'foo with params success';
+  ctx.status = 200;
+  done();
+  await next();
+});
+
+const defineEndpointFooCallback = done => testAppRouter.post('/foo/cb', async (ctx, next) => {
+  console.log('foo callback invoked!');
+  ctx.body = 'foo callback success';
+  ctx.status = 200;
+  done();
+  await next();
+});
+*/
+
+test.serial('POST /api/job/now with existing foo definition invokes the foo endpoint', async t => {
+  const fooInvoked = testAppCalled(defineEndpointFoo);
+  const res = await agendaAppRequest
+    .post('/api/job/now')
+    .send({name: 'foo'});
+
+  t.is(res.text, 'job scheduled for now');
+
+  return fooInvoked;
+});
+
+test.serial('POST /api/job/every with existing foo definition invokes the foo endpoint', async t => {
+  const fooInvoked = testAppCalled(defineEndpointFoo, 3);
+  const res = await agendaAppRequest
+    .post('/api/job/every')
+    .send({name: 'foo', interval: '10 miliseconds'});
+
+  t.is(res.text, 'job scheduled for repetition');
+
+  return fooInvoked;
 });
 
 test.serial('DELETE /api/job succeeds when a job is defined', async t => {
